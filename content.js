5f2c98c37513584df616545f96cfee6ba7667a50
DaleNaci <dale.nash01@gmail.com>
2019-12-06 17:51:29 -0500

    Updated code to add span

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
-let listOfTags=['p','a','h1','h2','h3','h4','h5','h6','i'];
+let listOfTags=['p','a','h1','h2','h3','h4','h5','h6','i','span'];
 listOfTags.forEach((tag)=>{
     let elements = document.getElementsByTagName(tag)
     for (let i = 0, l = elements.length; i < l; i++) {
-        elements[i].innerHTML="hiiiiiiiiiii"
+        elements[i].innerHTML="hiiiiiiiiiii";
     }
 });
