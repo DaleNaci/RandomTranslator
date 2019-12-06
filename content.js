ace35898cc2f9847c22603be2c24e0ae050240bb
Sam <student979@live.com>
2019-12-06 17:28:37 -0500

    now replaces more text

--- content.js
+++ content.js
@@ -1,4 +1,7 @@
-var paragraphs = document.getElementsByTagName('p');
-for (var i = 0, l = paragraphs.length; i < l; i++) {
-  paragraphs[i].innerHTML="hiiiiiiiiiii"
-}
+let listOfTags=['p','a','h1','h2','h3','h4','h5','h6','i'];
+listOfTags.forEach((tag)=>{
+    let elements = document.getElementsByTagName(tag)
+    for (let i = 0, l = elements.length; i < l; i++) {
+        elements[i].innerHTML="hiiiiiiiiiii"
+    }
+});
